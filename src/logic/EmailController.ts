1b4a1645ec844279d94ba122591a81424dd30a7f
Sergey Seroukhov <seroukhov@gmail.com>
2017-11-06 16:21:07 -0700

    Minor changes

--- src/logic/EmailController.ts
+++ src/logic/EmailController.ts
@@ -1,6 +1,6 @@
 let _ = require('lodash');
 let async = require('async');
-let mustache = require('mustache');
+let handlebars = require('handlebars');
 
 import { ConfigParams } from 'pip-services-commons-node';
 import { IConfigurable } from 'pip-services-commons-node';
@@ -144,11 +144,16 @@ export class EmailController implements IConfigurable, IReferenceable, ICommanda
         return "" + template;
     }
 
-    private renderTemplate(value: any, parameters: ConfigParams, language: string = 'en'): string {
+    private compileTemplate(value: any, language: string): any {
         let template = this.getLanguageTemplate(value, language);
-        return template ? mustache.render(template, parameters) : null;
+        return template ? handlebars.compile(template) : null;
     }
-    
+
+    private renderTemplate(value: any, parameters: ConfigParams, language: string = 'en'): string {
+        let template = this.compileTemplate(value, language);
+        return template ? template(parameters) : null;
+    }
+
     public sendMessage(correlationId: string, message: EmailMessageV1, parameters: ConfigParams,
         callback?: (err: any) => void): void {
         
