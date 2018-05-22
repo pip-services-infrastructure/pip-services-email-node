dedff7a80074411983413c5b5e35349e82b9cc2c
pipdeveloper <pipdevs@gmail.com>
2018-05-22 12:11:51 +0000

    project build by Travis CI [skip ci]

--- obj/src/container/EmailProcess.js
+++ obj/src/container/EmailProcess.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
+const pip_services_net_node_1 = require("pip-services-net-node");
+const pip_services_oss_node_1 = require("pip-services-oss-node");
 const EmailServiceFactory_1 = require("../build/EmailServiceFactory");
 class EmailProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("email", "Email delivery microservice");
         this._factories.add(new EmailServiceFactory_1.EmailServiceFactory);
+        this._factories.add(new pip_services_net_node_1.DefaultNetFactory);
+        this._factories.add(new pip_services_oss_node_1.DefaultOssFactory);
     }
 }
 exports.EmailProcess = EmailProcess;
--- obj/src/services/version1/EmailHttpServiceV1.js
+++ obj/src/services/version1/EmailHttpServiceV1.js
@@ -4,7 +4,7 @@ const pip_services_commons_node_1 = require("pip-services-commons-node");
 const pip_services_net_node_1 = require("pip-services-net-node");
 class EmailHttpServiceV1 extends pip_services_net_node_1.CommandableHttpService {
     constructor() {
-        super('email');
+        super('v1/email');
         this._dependencyResolver.put('controller', new pip_services_commons_node_1.Descriptor('pip-services-email', 'controller', 'default', '*', '1.0'));
     }
 }
