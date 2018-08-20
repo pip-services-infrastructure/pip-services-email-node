4d8a2fac72ffb0d51a7adefa100b6d1d183991d0
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:33:38 +0300

    added DefaultRpcFactory

--- obj/src/container/EmailProcess.js
+++ obj/src/container/EmailProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const EmailServiceFactory_1 = require("../build/EmailServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class EmailProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("email", "Email delivery microservice");
         this._factories.add(new EmailServiceFactory_1.EmailServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.EmailProcess = EmailProcess;
--- src/container/EmailProcess.ts
+++ src/container/EmailProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { EmailServiceFactory } from '../build/EmailServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class EmailProcess extends ProcessContainer {
 
     public constructor() {
         super("email", "Email delivery microservice");
         this._factories.add(new EmailServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
