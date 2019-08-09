9ded556270689e170448cef710d29975d589f9aa
pipdeveloper <pipdevs@gmail.com>
2019-08-09 18:41:16 +0000

    project build by Travis CI [skip ci]

--- obj/src/services/version1/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EmailHttpServiceV1_1 = require("./EmailHttpServiceV1");
-exports.EmailHttpServiceV1 = EmailHttpServiceV1_1.EmailHttpServiceV1;
-var EmailCommandableGrpcServiceV1_1 = require("./EmailCommandableGrpcServiceV1");
-exports.EmailCommandableGrpcServiceV1 = EmailCommandableGrpcServiceV1_1.EmailCommandableGrpcServiceV1;
-var EmailGrpcServiceV1_1 = require("./EmailGrpcServiceV1");
-exports.EmailGrpcServiceV1 = EmailGrpcServiceV1_1.EmailGrpcServiceV1;
-//# sourceMappingURL=index.js.map
